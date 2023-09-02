ea2660e5b206881dbfd0be718ce74c288786ef39
Adhii136 <121448026+Adhii136@users.noreply.github.com>
2023-09-02 19:17:27 +0530

    Add files via upload
    Write a program that takes side of a square as input and calculates the area of this square by using area() function.

---
+++ main.py
@@ -0,0 +1,10 @@
+def area(side):
+    squ_area = side * side
+    return squ_area
+
+side_leng = float(input("Enter the side length of the square: "))
+result = area(side_leng)
+print(f"The area of the square with side length {side_leng} is: {result}")
+
+
+
